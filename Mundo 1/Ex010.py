b58e8eab1b3023bdc5a5df2956d72d60263871c5
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2023-06-28 16:43:41 -0300

    first commit

---
+++ Mundo 1/Ex010.py
@@ -0,0 +1,3 @@
+valor = float(input('Quando tem na sua carteira?'))
+
+print(f'Com {valor:.2f}R$ você pode comprar {valor * 4.85:.2f}US.')
---
+++ Mundo 1/Ex011.py
@@ -0,0 +1,7 @@
+larg = float(input('Digite a largura da parede:'))
+alt = float(input('Digite a altura da parede:'))
+
+area = larg * alt
+
+print(f'Para pintar uma parede com {larg} m2 de largura e {alt} m2 de altura com uma área de {area} m2.')
+print(f'É necessário {area / 2}L de tinta.')
